afface0040cfeb1dcbad759b9c5f83874d18c5c3
Denis Kornilow <wkornilow@ananas.net.ua>
2016-12-10 19:05:11 +0200

    Added extension files

---
+++ dev/js/script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    console.log("ready!");
+};
+
+
+// var fHour,
+//     fMinute,
+//     lHour,
+//     lMinute,
+//     result,
+//     submit;
+//
+// fHour = $('#1');
+// fMinute = $('#2');
+// lHour = $('#3');
+// lMinute = $('#4');
+// result = $('#result');
+// submit = $('#submit');
+// submit.on('click', function () {
+//     var firstTime = (+fHour.val() * 60) + +fMinute.val();
+//     var secondTime = (+lHour.val() * 60) + +lMinute.val();
+//     result.val(+firstTime - +secondTime);
+//     console.log(firstTime);
+//     console.log(secondTime);
+//     console.log(result);
+// })
